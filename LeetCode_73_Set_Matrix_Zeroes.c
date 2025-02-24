5af2acc654e4138cb3960559cdd1db2c024e36a2
Ayush Gupta <g.ayush2k07@gmail.com>
2025-02-24 17:52:43 +0530

    Updates Set_Matrix_Zeroes.c

--- LeetCode_73_Set_Matrix_Zeroes.c
+++ LeetCode_73_Set_Matrix_Zeroes.c
@@ -10,7 +10,7 @@ void setZeros(int **matrix, int n, int m) {
         if (matrix[0][j]==0) firstRow=1;
     }
     for (int i=1;i<n;++i) {
-        for (int j=0;j<m;++j) {
+        for (int j=1;j<m;++j) {
             if (matrix[i][j]==0) {
                 matrix[0][j]=0;
                 matrix[i][0]=0;
